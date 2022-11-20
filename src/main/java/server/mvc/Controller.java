74b34f9653e892b2de5af1eef49b584b5481f0f0
Pavel <pavelromanov1226@gmail.com>
2022-11-20 20:32:04 +0300

    fix: add a class 'ServerHandler' with runnable intarface instead of static method so method 'onStartButtonClick' does not cause freezing of gui

--- src/main/java/server/mvc/Controller.java
+++ src/main/java/server/mvc/Controller.java
@@ -2,14 +2,9 @@ package server.mvc;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.SocketException;
 import java.util.regex.Pattern;
 
 public class Controller extends View {
@@ -24,29 +19,38 @@ public class Controller extends View {
 
     @FXML
     protected void onStartButtonClick() throws InterruptedException {
-        this.elListView.getItems().clear();
-        this.model.setShutDown(false);
-
-        this.btnStart.setDisable(true);
-        this.btnStop.setDisable(false);
-        main();
+        elListView.getItems().clear();
+        btnStart.setDisable(true);
+        btnStop.setDisable(false);
+        fieldPort.setDisable(true);
+        model.setShutDown(false);
+        new Thread(new ServerHandler()).start();
     }
 
     @FXML
     protected void onStopButtonClick() {
-        this.btnStart.setDisable(false);
-        this.btnStop.setDisable(true);
-        this.model.setShutDown(true);
+        btnStart.setDisable(false);
+        btnStop.setDisable(true);
+        fieldPort.setDisable(false);
+        model.setShutDown(true);
     }
 
-    private void main() throws InterruptedException {
-        this.fieldClientsConnected.setText(Integer.toString(Integer.parseInt(this.fieldClientsConnected.getText()) + 1));
-        int port = 6568;
-        if (isPort(this.fieldPort.getText())) {
-            port = Integer.parseInt(this.fieldPort.getText());
+    class ServerHandler implements Runnable {
+        @Override
+        public void run() {
+            int port = 6568;
+            if (isPort(fieldPort.getText())) {
+                port = Integer.parseInt(fieldPort.getText());
+            } else {
+                fieldPort.setText(Integer.toString(port));
+            }
+            model.setPort(port);
+            try {
+                model.main();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
-        this.model.setPort(port);
-        this.model.main();
     }
 
     public boolean isPort(String strNum) {
