896f91f10181ec1350a21589057b4dc24a398abf
Pavel <pavelromanov1226@gmail.com>
2022-11-27 17:58:55 +0300

    feat: add sql queries

---
+++ src/main/java/server/sqlQuery.java
@@ -0,0 +1,186 @@
+package server;
+
+
+import java.sql.*;
+public class sqlQuery {
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "toor";
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    public static final String SELECT_QUERY_DEPARTMENT = "SELECT * FROM department";
+    public static final String INSERT_QUERY_STATISTICS = "INSERT INTO statistics (app_name, app_time, comp_id) VALUES (?, ?, ?)";
+    public static final String SELECT_QUERY_STATISTICS = "SELECT * FROM statistics WHERE comp_id = ?";
+    public static final String UPDATE_QUERY_EQUIPMENT = "UPDATE equipment SET employee_id = ? WHERE comp_id = ?";
+    public static final String SELECT_QUERY_EQUIPMENT = "SELECT * FROM equipment";
+    public static final String INSERT_QUERY_EMPLOYEE = "INSERT INTO employee (username, lastname, firstname, password, department_id, position_id, comp_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    public static final String UPDATE_QUERY_EMPLOYEE = "UPDATE employee SET username = ?, lastname = ?, firstname = ?, password = ?, department_id = ?, position_id = ?, comp_id = ? WHERE employee_id = ?";
+    public static final String DELETE_QUERY_EMPLOYEE = "DELETE FROM employee WHERE employee_id = ?";
+    public static final String SELECT_QUERY_EMPLOYEE = "SELECT * FROM employee";
+    private static Statement statement;
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void disconnect() throws SQLException {
+        statement.close();
+        connection.close();
+    }
+
+    public void insertRecordStatistics(String app_name, String app_time, int comp_id) {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY_STATISTICS)) {
+            preparedStatement.setString(1, app_name);
+            preparedStatement.setString(2, app_time);
+            preparedStatement.setInt(3, comp_id);
+
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+            try {
+                preparedStatement.close();
+            } catch (SQLException ignored) {}
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    public void insertRecordEmployee(String username, String lastname, String firstname, String password, int department_id, int position_id, int comp_id) {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY_EMPLOYEE)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, lastname);
+            preparedStatement.setString(3, firstname);
+            preparedStatement.setString(4, password);
+            preparedStatement.setInt(5, department_id);
+            preparedStatement.setInt(6, position_id);
+            preparedStatement.setInt(7, comp_id);
+
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+            try {
+                preparedStatement.close();
+            } catch (SQLException ignored) {}
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    public void updateRecordEquipment(int employee_id, int comp_id) {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY_EQUIPMENT)) {
+            preparedStatement.setInt(1, employee_id);
+            preparedStatement.setInt(2, comp_id);
+
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+            try {
+                preparedStatement.close();
+            } catch (SQLException ignored) {}
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    public void updateRecordEmployee(String username, String lastname, String firstname, String password, int department_id, int position_id, int comp_id, int employee_id) {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY_EMPLOYEE)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, lastname);
+            preparedStatement.setString(3, firstname);
+            preparedStatement.setString(4, password);
+            preparedStatement.setInt(5, department_id);
+            preparedStatement.setInt(6, position_id);
+            preparedStatement.setInt(7, comp_id);
+            preparedStatement.setInt(8, employee_id);
+
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+            try {
+                preparedStatement.close();
+            } catch (SQLException ignored) {}
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    public void deleteRecordEmployee(int employee_id) {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY_EMPLOYEE)) {
+            preparedStatement.setInt(1, employee_id);
+
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+            try {
+                preparedStatement.close();
+            } catch (SQLException ignored) {}
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    public String selectAllDepartment() throws SQLException {
+        ResultSet resultSet = statement.executeQuery(SELECT_QUERY_DEPARTMENT);
+        StringBuilder result = new StringBuilder();
+        while (resultSet.next()) {
+            result.append(resultSet.getString(1)).append("; ").append(resultSet.getString(2)).append("; ").append(resultSet.getString(3)).append("\n");
+        }
+        resultSet.close();
+        if(result.toString().equals("")) {
+            result = new StringBuilder("\n");}
+        return result.toString();
+    }
+
+    public String selectAllEmployee() throws SQLException {
+        ResultSet resultSet = statement.executeQuery(SELECT_QUERY_EMPLOYEE);
+        StringBuilder result = new StringBuilder();
+        while (resultSet.next()) {
+            result.append(resultSet.getString(1)).append("; ").append(resultSet.getString(2)).append("; ").append(resultSet.getString(3)).append("; ").append(resultSet.getString(4)).append("; ").append(resultSet.getString(5)).append("; ").append(resultSet.getString(6)).append("; ").append(resultSet.getString(7)).append("; ").append(resultSet.getString(8)).append("\n");
+        }
+        resultSet.close();
+        if(result.toString().equals("")) {
+            result = new StringBuilder("\n");}
+        return result.toString();
+    }
+
+    public String selectAllEquipment() throws SQLException {
+        ResultSet resultSet = statement.executeQuery(SELECT_QUERY_EQUIPMENT);
+        StringBuilder result = new StringBuilder();
+        while (resultSet.next()) {
+            result.append(resultSet.getString(1)).append("; ").append(resultSet.getString(2)).append("; ").append(resultSet.getString(3)).append("; ").append(resultSet.getString(4)).append("\n");
+        }
+        resultSet.close();
+        if(result.toString().equals("")) {
+            result = new StringBuilder("\n");}
+        return result.toString();
+    }
+
+    public String selectAllStatistics() throws SQLException {
+        ResultSet resultSet = statement.executeQuery(SELECT_QUERY_STATISTICS);
+        StringBuilder result = new StringBuilder();
+        while (resultSet.next()) {
+            result.append(resultSet.getString(1)).append("; ").append(resultSet.getString(2)).append("; ").append(resultSet.getString(3)).append("; ").append(resultSet.getString(4)).append("\n");
+        }
+        resultSet.close();
+        if(result.toString().equals("")) {
+            result = new StringBuilder("\n");}
+        return result.toString();
+    }
+
+
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+}
